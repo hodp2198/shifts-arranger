820c58a2d50e59c67f2bd98f3b22b63e39ca4098
hodp2198 <hodp2198@users.noreply.github.com>
2026-06-03 14:04:46 +0300

    Configure Google OAuth client ID

--- src/googleConfig.js
+++ src/googleConfig.js
@@ -1,5 +1,5 @@
 (function () {
-  const GOOGLE_CLIENT_ID = "GOOGLE_CLIENT_ID.apps.googleusercontent.com";
+  const GOOGLE_CLIENT_ID = "667202878340-j1pv0e4hmac5rmtu8n1tqjbo69fve4i1.apps.googleusercontent.com";
   const GOOGLE_CALENDAR_SCOPE = "https://www.googleapis.com/auth/calendar.events";
 
   window.googleCalendarConfig = {
